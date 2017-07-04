0a185e2014a2664276c3cff2d65d84f0ccd9137a
Julien Lu <julien.luxiao@gmail.com>
2017-07-05 00:18:49 +0800

    [WIP] add controller

---
+++ code_gen/controller.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def __find_source_file():
+    pass
+
+def __filter_useles_func():
+    """
+    Filter the functions that are not used any place.
+    """
+    pass
+
+def __convert_func():
+    pass
+
+def __to_jsonresponse():
+    pass
+
+def gen_controller():
+    pass
