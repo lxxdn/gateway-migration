b4e685962f312118a528c3b06f061495d96e4ed6
陆骁 <xlu1@xiaohongshu.com>
2017-07-05 10:33:24 +0800

    import ast factory

--- ast_helpers/ast_factory.py
+++ ast_helpers/ast_factory.py
@@ -4,13 +4,17 @@
 import ast
 
 
-def create_ast_collector_visitor_class(visitor_type, conditions):
+def create_ast_collector_visitor_class(visitor_type, conditions=None):
     """
     This function aims to create a class to find all the node whose type is visitor_type
     and matches all the conditions
     """
 
     def general_visitor(self, node):
+        global conditions
+
+        if not conditions:
+            conditions = [lambda n: True]
         if reduce((lambda x, y: x and y), [cond(node) for cond in conditions]):
             self.nodes.append(node)
         ast.NodeVisitor.generic_visit(self, node)
--- code_gen/controller.py
+++ code_gen/controller.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-def __find_source_file():
-    pass
+from config import ConfigLoader
+from ast_factory import AstFilterFactory
+import os
+import ast
 
-def __filter_useles_func():
+def __find_ctlr_name_source(ctlr_name):
+    """
+    Return value is the class definition of the controller class
+    """
+
+    legacy_fulishe_root = ConfigLoader.data['fulishe-legace']['root']
+    for root, _, files in os.walk(legacy_fulishe_root):
+        for f_name in files:
+            if not f_name.endswith('.py'):
+                continue
+
+            with open(os.path.join(root, f_name)) as file:
+                content = file.read()
+                clazz = AstFilterFactory('ClassDef', [lambda node: node.name == ctlr_name])
+                visitor = clazz()
+                visitor.visit(ast.parse(content))
+                if visitor.nodes:
+                    return visitor.nodes[0]
+
+    return None
+
+
+def __filter_useles_func(class_def_node):
     """
     Filter the functions that are not used any place.
     """
+    visitor = AstFilterFactory('FunctionDef')()
+    visitor.visit(class_def_node)
+    func_nodes = visitor.nodes
+
+
+
+def __convert_func(func_nodes):
+    return __to_jsonresponse(func_nodes)
+
+
+def __to_jsonresponse(func_nodes):
     pass
 
-def __convert_func():
-    pass
-
-def __to_jsonresponse():
-    pass
 
 def gen_controller():
     pass
